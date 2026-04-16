9b3e9776f256e9a73b92771d6495305a9e6a4197
faithphoebe123 <faithpfbb-sm25@student.tarc.edu.my>
2026-04-16 14:24:03 +0800

    ConcessionItem.java

--- ConcessionItem.java
+++ ConcessionItem.java
@@ -1,30 +1,14 @@
 public class ConcessionItem {
-    private String itemId;
     private String name;
     private double price;
 
-    public ConcessionItem(String itemId, String name, double price) {
-        this.itemId = itemId;
+    public ConcessionItem(String name, double price) {
         this.name = name;
         this.price = price;
     }
 
-    public void createItem() {
-        System.out.println("Item created: " + name);
-    }
-
-    public void updateItem(String newName, double newPrice) {
-        this.name = newName;
-        this.price = newPrice;
-        System.out.println("Item updated.");
-    }
-
-    public void deleteItem() {
-        System.out.println("Item deleted: " + name);
-    }
-
-    public ConcessionItem getItem() {
-        return this;
+    public String getName() {
+        return name;
     }
 
     public double getPrice() {
