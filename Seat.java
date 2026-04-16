5c4188ea6ac93c3ecb351884ac4554c21fbdab60
lee <irleezyaerza@gmail.com>
2026-04-16 19:13:51 +0800

    Update Seat.java

--- Seat.java
+++ Seat.java
@@ -1,51 +1,56 @@
 
-
-public abstract class Seat { 
+public abstract class Seat {
 
     private String seatNumber;
     private String status;
     private double basePrice;
-    	
 
-    public Seat() { 
-		this("A0", "Available", 0.0);
+    public Seat() {
+        this("A0", "Available", 0.0);
     }
 
-    public Seat(String seatNumber, String status, double basePrice){
+    public Seat(String seatNumber, String status, double basePrice) {
         this.seatNumber = seatNumber;
         this.status = "Available";
         this.basePrice = basePrice;
     }
-    
-    public String getSeatNumber(){
-    	return seatNumber;
+
+    public boolean isBooked() {
+        return this.status.equalsIgnoreCase("Booked");
     }
-    
-    public void setSeatNumber(String seatNumber){
-    	this.seatNumber = seatNumber;
+
+    public void bookSeat() {
+        this.status = "Booked";
     }
-    
-    public String getStatus(){
-    	return status;
+
+    public String getSeatNumber() {
+        return seatNumber;
     }
-    
-    public void setStatus(String status){
-    	this.status = status;
+
+    public void setSeatNumber(String seatNumber) {
+        this.seatNumber = seatNumber;
     }
-    
-    public double getBasePrice(){
-    	return basePrice;
+
+    public String getStatus() {
+        return status;
     }
-    
-    public void setBasePrice(double basePrice){
-    	this.basePrice = basePrice;
+
+    public void setStatus(String status) {
+        this.status = status;
     }
-    
-    
+
+    public double getBasePrice() {
+        return basePrice;
+    }
+
+    public void setBasePrice(double basePrice) {
+        this.basePrice = basePrice;
+    }
+
     public abstract double calculatePrice();
-    
-    public String toString(){
-    	return String.format("Seat %s [%s] - Price: RM%.2f", seatNumber, status, calculatePrice());
+
+    public String toString() {
+        return String.format("Seat %s [%s] - Price: RM%.2f", seatNumber, status, calculatePrice());
     }
-    
-} 
+
+}
