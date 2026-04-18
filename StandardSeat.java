56b398eb8d7eeede75ca1318b561361643b240b5
lee <irleezyaerza@gmail.com>
2026-04-18 15:31:44 +0800

    Add default constructor to StandardSeat class

--- StandardSeat.java
+++ StandardSeat.java
@@ -1,13 +1,14 @@
-//ticket and seat booking mgmt
-//written by Irleesya
-
 public class StandardSeat extends Seat {
 	
+	public StandardSeat(){
+		this("A0", 0.0);
+	}
 	
 	public StandardSeat(String seatNumber, double basePrice){
 		super(seatNumber, "Available", basePrice);
 	}
 	
+	@Override
 	public double calculatePrice(){
 		return getBasePrice();
 	}
