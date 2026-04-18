fdea8559c0a5b831b09077c33f6e123cdaf9b641
lee <irleezyaerza@gmail.com>
2026-04-18 15:30:30 +0800

    Refactor Showtime class to use dateTime and hall

--- Showtime.java
+++ Showtime.java
@@ -1,72 +1,35 @@
 public class Showtime {
-    
-    // 属性
     private String showtimeId;
-    private String date;
-    private String time;
-    private Hall assignedHall;
-    private Movie movie; 
+    private Movie movie;
+    private String dateTime;
+    private Hall hall;
 
-    //构筑方法
-    public Showtime() {}
-
-    public Showtime(String showtimeId, String date, String time, Hall assignedHall, Movie movie) {
+    public Showtime(String showtimeId, Movie movie, String dateTime, Hall hall) {
         this.showtimeId = showtimeId;
-        this.date = date;
-        this.time = time;
-        this.assignedHall = assignedHall;
         this.movie = movie;
+        this.dateTime = dateTime;
+        this.hall = hall;
     }
 
-    // Getters 和 Setters
     public String getShowtimeId() {
         return showtimeId;
-    }
-
-    public void setShowtimeId(String showtimeId) {
-        this.showtimeId = showtimeId;
-    }
-
-    public String getDate() {
-        return date;
-    }
-
-    public void setDate(String date) {
-        this.date = date;
-    }
-
-    public String getTime() {
-        return time;
-    }
-
-    public void setTime(String time) {
-        this.time = time;
-    }
-
-    public Hall getAssignedHall() {
-        return assignedHall;
-    }
-
-    public void setAssignedHall(Hall assignedHall) {
-        this.assignedHall = assignedHall;
     }
 
     public Movie getMovie() {
         return movie;
     }
 
-    public void setMovie(Movie movie) {
-        this.movie = movie;
+    public String getDateTime() {
+        return dateTime;
     }
 
-    //toString方法
-	@Override
+    public Hall getHall() {
+        return hall;
+    }
+
+    @Override
     public String toString() {
-        return 
-        "Showtime ID: " + showtimeId
-      + "\nDate: " + date
-      + "\nTime: " + time
-      + "\nHall: " + (hall != null ? hall.getHallId() : "Not Assigned")
-      + "\nMovie: " + (movie != null ? movie.getTitle() : "Not Assigned");
+        return String.format("%s - %s | %s | Hall %d", 
+            showtimeId, movie.getTitle(), dateTime, hall.getHallId());
     }
 }
