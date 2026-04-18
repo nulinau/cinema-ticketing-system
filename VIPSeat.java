4a592afaf0d443940e00076a8310ee090a88e9a0
lee <irleezyaerza@gmail.com>
2026-04-18 15:32:05 +0800

    Add default constructor to VIPSeat class

--- VIPSeat.java
+++ VIPSeat.java
@@ -2,9 +2,13 @@ public class VIPSeat extends Seat{
 	
 	private double luxurySurcharge;
 	
+	public VIPSeat(){
+		this("A0", 0.0, 20.0);
+	}
+	
 	public VIPSeat(String seatNumber, double basePrice, double luxurySurcharge){
 		super(seatNumber, "Available", basePrice);
-		this.luxurySurcharge = luxurySurcharge;
+		this.luxurySurcharge = 20.0;
 	}
 	
 	public double calculatePrice(){
