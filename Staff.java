d27a511b98b364bd2c9c69939fb17b4a0b9c232d
eddietyk-sm25-ops <eddietyk-sm25@student.tarc.edu.my>
2026-04-20 05:49:24 +0800

    Update Staff.java

--- Staff.java
+++ Staff.java
@@ -23,34 +23,28 @@ public class Staff extends User {
         this.role = role;
     }
 
-    //方法
-    public boolean validateTicket(int ticket) {
-		// 先给一个默认返回值，保证编译通过
-        
-        System.out.println("Validating ticket...");
-        return true;
-        
-        //需要取决于irleesya怎么写，先置空
-    }
-
-    public String reportGenerate(Order order, Hall hall, Movie movie) {
-        String Report_CustomerName =   getUserName();
-        String Report_OrderId      =   String.valueOf(order.getOrderId());//class 未定义
-		String Report_OrderDate    =   String.valueOf(order.getOrderDate());//class 未定义
-		String Report_TotalAmount  =   String.valueOf(order.getTotalAmount());;//class 未定义
-		String Report_HallId       =   String.valueOf(hall.getHallId());//class 未定义
-		String Report_MoviveName   =   String.valueOf(movie.getTitle());//class 未定义
+    public String reportGenerate(Order order) {
+        String Report_CustomerName =   String.valueOf(order.getOrderByCustomer().getUserName());
+        String Report_OrderId      =   String.valueOf(order.getOrderId());
+		String Report_OrderDate    =   String.valueOf(order.getOrderDate());
+		String Report_TotalAmount  =   String.valueOf(order.getTotalAmount());
+		String Report_HallId       =   String.valueOf(order.getHall().getHallId());
+		String Report_MovieName   =   String.valueOf(order.getMovie().getTitle());
 		
-        Report newReport = new Report(Report_CustomerName, Report_OrderId, Report_OrderDate, Report_TotalAmount, Report_HallId, Report_MoviveName);
+        Report newReport = new Report(Report_CustomerName, Report_OrderId, Report_OrderDate, Report_TotalAmount, Report_HallId, Report_MovieName);
         
-        return newReport.toString(); 
+        
+        String finalReport = newReport.toString(); 
+        System.out.print(finalReport); 
+        return finalReport;
     }
 
     // toString 方法 (用于显示 Staff 自己的信息)
     @Override
     public String toString() {
         return super.toString() 
-        	  +"\nCustomer type: Staff"
-              +"\nRole: " + role;
+        	  +"\nUser type : Staff"
+              +"\nRole      : " + role
+              +"----------------------------------------\n";
     }
 }
